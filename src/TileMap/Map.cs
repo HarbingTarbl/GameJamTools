84835c9f353233cf12ba8e31e4f6ba23563e3bc1
Jason Spafford <nullprogrammer@gmail.com>
2013-01-24 21:40:19 -0800

    Added Tiled TMX support for Jammy

---
+++ src/TileMap/Map.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Jammy.TileMap
+{
+	public class Tile
+	{
+		public Texture2D Texture;
+		public Rectangle SourceRectangle;
+		public SpriteEffects SpriteEffects;
+	}
+
+	public class Layer
+	{
+		public int Width;
+		public int Height;
+		public Tile[] Tiles;
+	}
+
+	public class Map
+	{
+		public int TileWidth;
+		public int TileHeight;
+		public List<Layer> Layers = new List<Layer> ();
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			foreach (var l in Layers)
+			{
+				spriteBatch.Begin ();
+
+				for (int y = 0; y < l.Height; y++)
+				{
+					for (int x = 0; x < l.Width; x++)
+					{
+						Tile t = l.Tiles[y * l.Width + x];
+						spriteBatch.Draw (
+							t.Texture,
+							new Rectangle (x * TileWidth, y * TileHeight, TileWidth, TileHeight),
+							t.SourceRectangle,
+							Color.White,
+							0,
+							Vector2.Zero,
+							t.SpriteEffects,
+							0);
+					}
+				}
+
+				spriteBatch.End ();
+			}
+		}
+	}
+}
